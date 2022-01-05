5d265c40dc42e851f1ce2f45eda6bbbc4e9ced2b
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-01-05 22:49:34 +0900

    ページングとプルドリフレッシュの実装

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  RefreshControl,
+} from 'react-native';
 import ListItem from '../components/ListItem';
 import Loading from '../components/Loading';
 import Constants from 'expo-constants';
@@ -19,20 +24,46 @@ export default function HomeScreen(props) {
 
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const pageRef = useRef(1);
+  const fetchedAllRef = useRef(false);
 
   useEffect(() => {
-    fetchArticles();
+    setLoading(true);
+    fetchArticles(1);
+    setLoading(false);
   }, []);
 
-  const fetchArticles = async () => {
-    setLoading(true);
+  const fetchArticles = async (page) => {
     try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
+      const response = await axios.get(`${URL}&page=${page}`);
+      if (response.data.articles.length > 0) {
+        setArticles((prevArticles) => [
+          ...prevArticles,
+          ...response.data.articles,
+        ]);
+      } else {
+        fetchedAllRef.current = true;
+      }
     } catch (error) {
       console.log(error);
     }
-    setLoading(false);
+  };
+
+  const onEndReached = () => {
+    if (!fetchedAllRef.current) {
+      pageRef.current = pageRef.current + 1;
+      fetchArticles(pageRef.current);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    setArticles([]);
+    pageRef.current = 1;
+    fetchedAllRef.current = false;
+    await fetchArticles(1);
+    setRefreshing(false);
   };
 
   return (
@@ -48,6 +79,10 @@ export default function HomeScreen(props) {
           />
         )}
         keyExtractor={(item, index) => index.toString()}
+        onEndReached={onEndReached}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
       {loading && <Loading />}
     </SafeAreaView>
